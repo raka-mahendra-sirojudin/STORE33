c89e370c8abd3cb2276221f5e746f17678870f5d
Raka Mahendra <familylink468@gmail.com>
2026-07-24 08:24:31 +0700

    Add files via upload
    Signed-off-by: Raka Mahendra <familylink468@gmail.com>

---
+++ Latihan Soal.js
@@ -0,0 +1,27 @@
+let nama = "Raka";
+let umur = 17;
+console.log("Nama saya " + nama + " dan saya berumur " + umur + " tahun");
+
+
+let nilai = 90;
+if (nilai >= 90) {
+    console.log("lulus");
+} else {
+    console.log("tidak lulus");
+}
+
+
+for (let i = 1; i <= 20; i++) {
+
+    console.log(i);
+
+}
+
+
+function menghitungpersegipanjang(panjang, lebar) {
+
+    return panjang * lebar;
+
+}
+
+console.log(menghitungpersegipanjang(10, 5));
